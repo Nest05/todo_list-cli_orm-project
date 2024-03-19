209e4ed0c19b3055d0c2a6de37835f0633dfe67a
Nestor Masinde <nestormasinde@gmail.com>
2024-03-20 00:48:47 +0300

    feat: create models and database with tables

---
+++ lib/db/models.py
@@ -0,0 +1,49 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, create_engine
+from sqlalchemy.orm import relationship, declarative_base, sessionmaker
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id_ = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
+    todos = relationship('ToDo', backref='user', cascade='all, delete-orphan')
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id_ = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    todos = relationship('ToDo', backref = 'category')
+
+class DueDate(Base):
+    __tablename__ = 'due_dates'
+
+    id_ = Column(Integer, primary_key=True)
+    date = Column(DateTime, nullable=False)
+
+    todos =relationship('ToDo', backref='due_date')
+
+
+class ToDo(Base):
+    __tablename__ = 'todos'
+
+    id_ = Column(Integer, primary_key=True)
+    task = Column(String, nullable=False)
+
+    user_id = Column(Integer, ForeignKey('users.id_'), nullable=False)
+    category_id = Column(Integer, ForeignKey('categories.id_'), nullable=False)
+    due_date_id = Column(Integer, ForeignKey('due_dates.id_'), nullable=False)
+
+    category = relationship('Category', backref='todos')
+    due_date = relationship('DueDate', backref='todos')
+
+
+engine = create_engine('sqlite:///todo.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
