78b737369ba832d1860bcef34deea756897ad60f
Nestor Masinde <nestormasinde@gmail.com>
2024-03-21 01:41:11 +0300

    feat: Add helper

---
+++ lib/cli.py
@@ -0,0 +1,34 @@
+from helpers import (
+    exit_program,
+    add_todo
+)
+
+def main():
+    while True:
+        menu()
+        choice = input("-> ")
+        if choice == "0":
+            exit_program()
+        elif choice == "1":
+            add_todo()
+            return_or_continue()
+
+
+def menu():
+    print("Welcome to your trusted todo manager!")
+    print("Select what you would like to do: ")
+    print("0. Exit the manager")
+    print("1. Add a todo")
+
+def return_or_continue():
+    print("Would you like to return to the menu or continue?")
+    print("1. Return to the menu")
+    print("2. Continue")
+    choice = input("> ")
+    if choice == "1":
+        return
+    elif choice == "2":
+        pass
+
+if __name__ == "__main__":
+    main()
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,37 +1,53 @@
 from db.models import User, ToDo, Category, DueDate, session
 from datetime import datetime
 from getpass import getpass
+from quote import quote
+from py_random_words import RandomWords
+
+random_word = RandomWords()
+w = random_word.get_word()
+result = quote(w, limit=1)
+
+
+
+def exit_program():
+    print(result[0]['quote'])
+    print("************************************")
+    print(f"Quote by -> {result[0]['author']}")
+    exit()
 
 def add_todo():
-    # Retrieve existing users from the databse
-    users = session.query(User).all()
-    if not users:
-        print("No users found. Please create a user first.")
-        return
-    
-    # Prompt the user to select a user
-    print("Select a user:")
-    for index, user in enumerate(users):
-        print(f"{index+1}. {user.username}")
-    user_choice = input("Enter the user number: ")
-    user_index = int(user_choice) - 1
-
-    # Validate the user choice
-    if user_index < 0 or user_index >= len(users):
-        print("Invalid user selection.")
-        return
-
-
-    user = users[user_index]
-    password = getpass("Enter your password: ")
-    task = input("Enter the task: ")
-    category_name = input("Enter the category: ")
-    due_date_str = input("Enter the due date (YYYY-MM-DD HH:MM:SS): ")
-
-
     try:
-        # Retrieve category and due date objects based on input values
+        # Retrieve existing users from the databse
+        users = session.query(User).all()
+        if not users:
+            print("No users found. Please create a user first.")
+            return
+        
+        # Prompt the user to select a user
+        print("Select a user:")
+        for index, user in enumerate(users):
+            print(f"{index+1}. {user.username}")
+        user_choice = input("Enter the user number: ")
+        user_index = int(user_choice) - 1
+
+        # Validate the user choice
+        if user_index < 0 or user_index >= len(users):
+            print("Invalid user selection.")
+            return
+
+
+        user = users[user_index]
+        password = getpass("Enter your password: ")
+
+
+        # Validate the password
         if user.password == password:
+            task = input("Enter the task: ")
+            category_name = input("Enter the category: ")
+            due_date_str = input("Enter the due date (YYYY-MM-DD HH:MM:SS): ")
+
+            # Retrieve category and due date objects based on input values 
             category = session.query(Category).filter_by(name=category_name).first()
             due_date = DueDate(date=datetime.strptime(due_date_str, "%Y-%m-%d %H:%M:%S"))
 
@@ -42,4 +58,4 @@ def add_todo():
     except Exception as exc:
         print("Error creating todo: ", exc)
 
-add_todo()
+# add_todo()
